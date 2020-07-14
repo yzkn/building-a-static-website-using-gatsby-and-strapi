0e2fc98d60526f04858f88a96daf81ed0eadfe9e
YA <YA-androidapp@users.noreply.github.com>
2020-07-14 12:44:45 +0900

    Install the Strapi source plugin

--- tutorial/blog/gatsby-config.js
+++ tutorial/blog/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Our blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `YA-androidapp`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +11,17 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
+          'article',
+          'user'
+        ],
+        queryLimit: 1000,
       },
     },
     `gatsby-transformer-sharp`,
@@ -30,5 +41,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    'gatsby-plugin-offline',
   ],
 }
